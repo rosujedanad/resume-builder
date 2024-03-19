b10f169cb5f7a3db055d7521b5ae5874be6ab924
alentitude <122140636+alentitude@users.noreply.github.com>
2024-03-19 18:04:45 +0530

    added home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,92 +2,113 @@ import React, { useState } from "react";
 import Loading from "./Loading";
 
 const Home = () => {
-    const [fullName, setFullName] = useState("");
-    const [currentPosition, setCurrentPosition] = useState("");
-    const [currentLength, setCurrentLength] = useState(1);
-    const [currentTechnologies, setCurrentTechnologies] = useState("");
-    const [headshot, setHeadshot] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        console.log({
-            fullName,
-            currentPosition,
-            currentLength,
-            currentTechnologies,
-            headshot,
-        });
-        setLoading(true);
-    };
-    //👇🏻 Renders the Loading component you submit the form
-    if (loading) {
-        return <Loading />;
-    }
-    return (
-        <div className='app'>
-            <h1>Resume Builder</h1>
-            <p>Generate a resume with ChatGPT in few seconds</p>
-            <form
-                onSubmit={handleFormSubmit}
-                method='POST'
-                encType='multipart/form-data'
-            >
-                <label htmlFor='fullName'>Enter your full name</label>
-                <input
-                    type='text'
-                    required
-                    name='fullName'
-                    id='fullName'
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                />
-                <div className='nestedContainer'>
-                    <div>
-                        <label htmlFor='currentPosition'>Current Position</label>
-                        <input
-                            type='text'
-                            required
-                            name='currentPosition'
-                            className='currentInput'
-                            value={currentPosition}
-                            onChange={(e) => setCurrentPosition(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='currentLength'>For how long? (year)</label>
-                        <input
-                            type='number'
-                            required
-                            name='currentLength'
-                            className='currentInput'
-                            value={currentLength}
-                            onChange={(e) => setCurrentLength(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='currentTechnologies'>Technologies used</label>
-                        <input
-                            type='text'
-                            required
-                            name='currentTechnologies'
-                            className='currentInput'
-                            value={currentTechnologies}
-                            onChange={(e) => setCurrentTechnologies(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <label htmlFor='photo'>Upload your headshot image</label>
-                <input
-                    type='file'
-                    name='photo'
-                    required
-                    id='photo'
-                    accept='image/x-png,image/jpeg'
-                    onChange={(e) => setHeadshot(e.target.files[0])}
-                />
-                <button>CREATE RESUME</button>
-            </form>
+  const [fullName, setFullName] = useState("");
+  const [currentPosition, setCurrentPosition] = useState("");
+  const [currentLength, setCurrentLength] = useState(1);
+  const [currentTechnologies, setCurrentTechnologies] = useState("");
+  const [headshot, setHeadshot] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      fullName,
+      currentPosition,
+      currentLength,
+      currentTechnologies,
+      headshot,
+    });
+    setLoading(true);
+  };
+  //👇🏻 Renders the Loading component you submit the form
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div className="app">
+      <div className="detbox">
+        <div className="boxdet"></div>
+        <div className="boxmain">
+          <h1 className="Mtitle">Candidate Credentials</h1>
+          <p className="desc">
+            Please enter your credentials to generate your resume.
+          </p>
+          <form
+            onSubmit={handleFormSubmit}
+            method="POST"
+            encType="multipart/form-data"
+          >
+            <label htmlFor="fullName" className="heading">
+              Full Name:
+            </label>
+            <input
+              type="text"
+              required
+              name="fullName"
+              id="fullName"
+              className="fields1"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              placeholder="Eg: John Doe"
+            />
+
+            <div>
+              <label htmlFor="currentPosition" className="heading">
+                Contact Number:
+              </label>
+              <input
+                type="text"
+                required
+                name="currentPosition"
+                className="fields1"
+                value={currentPosition}
+                placeholder="+XX-XXXXXXXXX"
+                onChange={(e) => setCurrentPosition(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="currentLength" className="heading">
+                For how long? (year)
+              </label>
+              <input
+                type="number"
+                required
+                name="currentLength"
+                className="fields1"
+                value={currentLength}
+                onChange={(e) => setCurrentLength(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="currentTechnologies" className="heading">
+                Technologies used
+              </label>
+              <input
+                type="text"
+                required
+                name="currentTechnologies"
+                className="fields1"
+                value={currentTechnologies}
+                onChange={(e) => setCurrentTechnologies(e.target.value)}
+              />
+            </div>
+
+            <label htmlFor="photo" className="heading">
+              Upload your headshot image
+            </label>
+            <input
+              className="fields1"
+              type="file"
+              name="photo"
+              required
+              id="photo"
+              accept="image/x-png,image/jpeg"
+              onChange={(e) => setHeadshot(e.target.files[0])}
+            />
+            <button className="mbutton">Create Resume</button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default Home;
