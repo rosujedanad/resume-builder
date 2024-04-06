7dc5b367b8c10b7742aeb33caaee5dc5b320c2be
alentitude <122140636+alentitude@users.noreply.github.com>
2024-04-06 09:12:07 +0530

    Routing Error of Form Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/form/Form";
 import Resume from "./components/Resume";
 import Signin from "./components/signin/Signin";
 import { Navigate } from "react-router-dom";
@@ -8,39 +8,30 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import Loading from "./components/loading/Loading";
 
 const App = () => {
-    // Retrieve authentication status from localStorage on component mount
-    const [isAuthenticated, setIsAuthenticated] = useState(() => {
-      const storedAuthStatus = localStorage.getItem('isAuthenticated');
-      return storedAuthStatus ? JSON.parse(storedAuthStatus) : false;
-    });
-  
-  
-    const setAuthStatus = (status) => {
-      setIsAuthenticated(status);
-      localStorage.setItem('isAuthenticated', JSON.stringify(status));
-    };
-  
-    return (
-      <div className="App">
-        <GoogleOAuthProvider clientId="480714727278-491bm46a67efiq09ol4soij31irjiio4.apps.googleusercontent.com">
-        <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<Signin/>}
-          />
-          <Route
-            path="/loading"
-            element={<Loading/>}
-          />
-          <Route
-            path="/home"
-            element={<Home/>}/>
-        </Routes>
-        </BrowserRouter>
-        </GoogleOAuthProvider>
-      </div>
-    );
+  // Retrieve authentication status from localStorage on component mount
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    const storedAuthStatus = localStorage.getItem("isAuthenticated");
+    return storedAuthStatus ? JSON.parse(storedAuthStatus) : false;
+  });
+
+  const setAuthStatus = (status) => {
+    setIsAuthenticated(status);
+    localStorage.setItem("isAuthenticated", JSON.stringify(status));
   };
-  
-  export default App;
+
+  return (
+    <div className="App">
+      <GoogleOAuthProvider clientId="480714727278-491bm46a67efiq09ol4soij31irjiio4.apps.googleusercontent.com">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Signin />} />
+            <Route path="/loading" element={<Loading />} />
+            <Route path="/form" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </div>
+  );
+};
+
+export default App;
