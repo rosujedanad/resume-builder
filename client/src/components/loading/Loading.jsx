50c218fe1a9b7aa307ccbfebdeef00038cb58e87
Rosu J Edanad <rosujedanad@gmail.com>
2024-04-06 11:49:30 +0530

    changes

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -4,9 +4,9 @@ const Loading = () => {
     return (
         <div className={styles.application}>
             <div className={styles.centering}>
-          <img className={styles.imagefile} src="../../src/assets/loadingil.jpeg" alt="loading"/>
-          <h3 className={styles.restext}>Generating your resume.</h3>
-          <h3 className={styles.loadtext}>Please wait.....</h3>
+          <img className={styles.imagefile} src="../../src/assets/loadings.png" alt="loading"/>
+          <h3 className={styles.restext}>Generating your resume</h3>
+          <h3 className={styles.loadtext}>Please wait .....</h3>
             </div>
             
         </div>
