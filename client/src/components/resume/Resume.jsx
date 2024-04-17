9326602a9bea1f3288065ff6dbef8607f980abc2
alentitude <122140636+alentitude@users.noreply.github.com>
2024-04-17 13:55:04 +0530

    Resume Page desing

--- client/src/components/resume/Resume.jsx
+++ client/src/components/resume/Resume.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ErrorPage from "../error/ErrorPage";
+import styles from "./styles.module.css";
 import { useLocation } from "react-router-dom";
 
 const Resume = () => {
@@ -9,9 +10,39 @@ const Resume = () => {
   console.log(formData);
   return (
     <>
-      <button onClick={handlePrint}>Print Page</button>
-      <main className="container">
-        <p>{formData.details.name}</p>
+      <main className={styles.container}>
+        <div className={styles.resume}>
+          <div className={styles.header}>
+            <div className={styles.canName}>
+              <p>{formData.details.name}</p>
+            </div>
+            <div className={styles.address}>
+              <p className={styles.addText}>
+                {formData.contact.place} . {formData.contact.state} .{" "}
+                {formData.contact.mobile} . {formData.contact.email}
+                {/* { . {formData.contact.linkedin} .{" "}
+                {formData.contact.github} */}
+              </p>
+            </div>
+          </div>
+          <div className={styles.resBody}>
+            <div className={styles.about}>
+              <h2 className={styles.resTitle}>Candidate Profile</h2>
+              <p className={styles.resTextGen}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                auctor venenatis felis, non ultricies odio faucibus ac.
+                Curabitur vestibulum erat nec ligula posuere, in rutrum magna
+                mattis. Sed suscipit, ante eget suscipit eleifend, velit libero
+                pellentesque libero.
+              </p>
+            </div>
+            <div className="detCol">
+              <div className={styles.colLeft}></div>
+              <div className={styles.colRight}></div>
+            </div>
+          </div>
+        </div>
+        <button onClick={handlePrint}>Print Page</button>
       </main>
     </>
   );
