e7f38392562604c9542bef03395abcf4978d101c
alentitude <122140636+alentitude@users.noreply.github.com>
2024-04-22 14:26:30 +0530

    ongoing

--- server/src/models/eduModel.js
+++ server/src/models/eduModel.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose');
-const mongooseDelete = require('mongoose-delete');
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+
 const eduDataSchema = new mongoose.Schema({
-    UserID :{
-        type : String,
-        required : true
+  UserID: {
+    type: String,
+    required: true,
+  },
+  resumeID: {
+    type: String,
+    required: true,
+  },
+  education: [
+    {
+      college: { type: String, required: true },
+      department: { type: String, required: true },
+      cgpa: { type: String, required: true },
+      school: { type: String, required: true },
+      stream: { type: String, required: true },
+      percentage: { type: Number, required: true },
     },
-    resumeID :{
-        type : String,
-        required : true
-    },
-    ug: {
-            "college" : {type: String, required: true},
-            "department" : {type: String, required: true},
-            "cgpa" : {type: String, required: true},
-    },
-
-    hss : {
-    
-            "school": {type: String, required: true},
-            "stream": {type: String, required: true},
-            "percentage": {type: Number, required: true}
-        }
-
-    
+  ],
 });
 
-eduDataSchema.plugin(mongooseDelete, { overrideMethods: 'all' });
-const eduData = mongoose.model('eduData', eduDataSchema);
+eduDataSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+const eduData = mongoose.model("eduData", eduDataSchema);
 
 module.exports = eduData;
